d65a7fe8d706c826c5c0586d581aa662d645b3e3
mounika <reddymounika2805@gmail.com>
2023-05-06 19:51:26 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-function App() {
+function SignupForm() {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [mobileNumber, setMobileNumber] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({});
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit} className="signup-form">
+      <div className="form-group">
+        <label htmlFor="firstName">First Name *</label>
+        <input
+          type="text"
+          id="firstName"
+          name="firstName"
+          value={firstName}
+          onChange={(e) => setFirstName(e.target.value)}
+          required
+          minLength={5}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="lastName">Last Name</label>
+        <input
+          type="text"
+          id="lastName"
+          name="lastName"
+          value={lastName}
+          onChange={(e) => setLastName(e.target.value)}
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="mobileNumber">Mobile Number *</label>
+        <input
+          type="tel"
+          id="mobileNumber"
+          name="mobileNumber"
+          value={mobileNumber}
+          onChange={(e) => setMobileNumber(e.target.value)}
+          pattern="[0-9]{10}"
+          maxLength={10}
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="email">Email Address *</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </div>
+
+      <div className="form-group">
+        <label htmlFor="password">Password *</label>
+        <input
+          type="password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+          minLength={5}
+          pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{5,}$"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">Signup</button>
+    </form>
   );
 }
 
-export default App;
+export default SignupForm;
